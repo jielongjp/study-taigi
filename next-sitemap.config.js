4204943ed8dd3afef80f5aaaa73d84080ed7ec4c
jielongjp <jdan000@gmail.com>
2023-10-12 21:23:38 +0900

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.learntaigi.com",
+  changefreq: "weekly",
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  // ...other options
+};
