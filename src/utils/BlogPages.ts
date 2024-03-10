7f63fdff7d13399d9cf84adeb3f9ed0fba43de52
“jielongjp” <82949450+jielongjp@users.noreply.github.com>
2024-03-10 19:42:25 +0000

    add blog page

--- src/utils/BlogPages.ts
+++ src/utils/BlogPages.ts
@@ -6,16 +6,24 @@ interface BlogPage {
   preview?: string;
 }
 
-const BlogPages: BlogPage[]  = [
+const BlogPages: BlogPage[] = [
   {
-    slug: 'resources',
-    title: 'Resources for learning Taiwanese',
-    preview: "While there aren't a lot of resources, here are a few standout books and sites are available for learning Taiwanese Hokkien in English."
-  }, 
+    slug: "resources",
+    title: "Resources for learning Taiwanese",
+    preview:
+      "While there aren't a lot of resources, here are a few standout books and sites are available for learning Taiwanese Hokkien in English.",
+  },
   {
-    slug: 'intermediate-taiwanese',
-    title: 'How to Learn Intermediate Taiwanese',
-    preview: 'Taiwanese resources are few and far between and advanced material for learning Taiwanese is even scarcer. Here is a handy list to help you out.'
+    slug: "intermediate-taiwanese",
+    title: "How to Learn Intermediate Taiwanese",
+    preview:
+      "Taiwanese resources are few and far between and advanced material for learning Taiwanese is even scarcer. Here is a handy list to help you out.",
+  },
+  {
+    slug: "how-to-start-learning-taiwanese",
+    title: "How to Start Learning Taiwanese",
+    preview:
+      "Want to learn Taiwanese Hokkien but don't know where to start? this guide will begin your Taiwanese language journey.",
   },
 ];
 
