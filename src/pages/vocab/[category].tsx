3d49fe86eaa2c318367f3164a72f1a5fb7f60ef2
“jielongjp” <82949450+jielongjp@users.noreply.github.com>
2024-03-31 12:57:09 +0100

    add frequency lists

--- src/pages/vocab/[category].tsx
+++ src/pages/vocab/[category].tsx
@@ -12,7 +12,8 @@ interface VocabPageProps {
 }
 
 const VocabPage: React.FC<VocabPageProps> = ({ categoryName }) => {
-  const spreadsheetUrl = CategoryNames[categoryName];
+  const spreadsheetUrl = CategoryNames[categoryName].url;
+  const is_freq = CategoryNames[categoryName].is_freq;
   const categoryNameSpaces = categoryName.replace(/_/g, " ");
   const metaDescription = `Taiwanese vocabulary list for ${categoryNameSpaces}. Learn ${categoryNameSpaces} words in Taiwanese Hokkien with English and Mandarin`;
 
@@ -28,18 +29,75 @@ const VocabPage: React.FC<VocabPageProps> = ({ categoryName }) => {
       </Head>{" "}
       <StContainer>
         <StTitle>Taiwanese Hokkien Vocab</StTitle>
+        {is_freq && (
+          <>
+            <StTitle>{categoryNameSpaces} words</StTitle>
+            <StTextContainer>
+              <StParagraph>
+                This vocabulary list has been made from a list of the top 5000
+                Mandarin Chinese words which was used to search and convert to
+                the Taiwanese term via sutian.moe.edu.tw. <br /> Due to the
+                number of words it has not been completely sorted and the
+                English translation for this particular list is mostly machine
+                translation. <br />
+                <br />
+                There are not many frequency based vocabulary lists available
+                for Taiwanese Hokkien so I have made and released this one,
+                however use it with care, it will most likely be missing some
+                common idiomatic Taiwanese and Taiwanese words that don&apos;t
+                translate directly with Mandarin. Note also that each Mandarin
+                search term only gets one Taiwanese translation. <br /> <br />
+                With no simple method to programmatically decide which result
+                from sutian.moe.edu.tw is the most common, only the first one is
+                used. This may cause some less common translations to get mixed
+                in some cases so be aware. Rest assured the translations will be
+                correct, due to the great work done at sutian.moe.edu.tw. At
+                some time I will try to sort through this list and edit the
+                above issues.
+                <br /> <br />
+                As there are a large number of words in these lists, it may take
+                longer to load.
+              </StParagraph>
+            </StTextContainer>
+            <StLinkContainer>
+              <StLink>
+                <Link href="/vocab/top_4000">top 4000</Link>
+              </StLink>
+              <StLink>
+                <Link href="/vocab/top_500">top 500</Link>
+              </StLink>
+              <StLink>
+                <Link href="/vocab/top_500_to_1000">top 500 to 1000</Link>
+              </StLink>
+              <StLink>
+                <Link href="/vocab/top_1000_to_1500">top 1000 to 1500</Link>
+              </StLink>
+              <StLink>
+                <Link href="/vocab/top_1500_to_2000">top 1500 to 2000</Link>
+              </StLink>
+              <StLink>
+                <Link href="/vocab/top_2000_to_3000">top 2000 to 3000</Link>
+              </StLink>
+              <StLink>
+                <Link href="/vocab/top_3000_to_4000">top 3000 to 4000</Link>
+              </StLink>
+            </StLinkContainer>
+          </>
+        )}
         <Link href="/vocab">See all categories</Link>
         <DataList spreadsheetUrl={spreadsheetUrl} categoryName={categoryName} />
-        <StTextContainer>
-          <StParagraph>
-            List of Taiwanese Hokkien vocabulary for {categoryNameSpaces}{" "}
-            related words. Click the audio play button to listen to
-            pronunciation. Vocab cards with example sentences will have a button
-            that can be clicked to show examples for each word. Use the
-            &apos;Test me&apos; to test yourself on each of the words and master
-            the {categoryNameSpaces} vocabulary for Taiwanese.
-          </StParagraph>
-        </StTextContainer>
+        {!is_freq && (
+          <StTextContainer>
+            <StParagraph>
+              List of Taiwanese Hokkien vocabulary for {categoryNameSpaces}{" "}
+              related words. Click the audio play button to listen to
+              pronunciation. Vocab cards with example sentences will have a
+              button that can be clicked to show examples for each word. Use the
+              &apos;Test me&apos; to test yourself on each of the words and
+              master the {categoryNameSpaces} vocabulary for Taiwanese.
+            </StParagraph>
+          </StTextContainer>
+        )}
       </StContainer>
     </>
   );
@@ -54,8 +112,35 @@ const StContainer = styled.div`
 `;
 
 const StTextContainer = styled.div`
-  margin: 0 24px;
+  margin: 0 10%;
   text-align: left;
+`;
+
+const StLinkContainer = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  @media (max-width: 650px) {
+    flex-direction: column;
+  }
+`;
+
+const StLink = styled.button`
+  background-color: #f0f0f0;
+  padding: 10px 20px;
+  margin: 5px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+
+  &:hover {
+    background-color: #85a3c3;
+  }
 `;
 
 export async function getStaticPaths() {
--- src/utils/CatergoryNames.ts
+++ src/utils/CatergoryNames.ts
@@ -1,48 +1,104 @@
+interface Category {
+  url: string;
+  is_freq?: boolean;
+}
+
 interface CategoryName {
-  [category: string]: string; // spreadsheets are publicly posted so no need for env
+  [category: string]: Category;
 }
 
 const CategoryNames: CategoryName = {
-  taiwan_food:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1814397280&single=true",
-  colors:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=70370957&single=true",
-  fruit_and_veg:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1626957566&single=true",
-  hobbies:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1225906239&single=true",
-  family:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=0&single=true",
-  animals:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=934038451&single=true",
-  places:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1557824374&single=true",
-  taste:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=610825890&single=true",
-  home: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1530419016&single=true",
-  weather:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1054642637&single=true",
-  body: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1899740434&single=true",
-  clothes:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1167911748&single=true",
-  emotions:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1312510160&single=true",
-  transport:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=702917173&single=true",
-  school:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=213711171&single=true",
-  travel:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1043842277&single=true",
-  adjectives:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=531229956&single=true",
-  christmas:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=312614104&single=true",
-  days_of_the_week:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1757834596&single=true",
-  technology:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=173773526&single=true",
-  sports:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1028551617&single=true",
+  taiwan_food: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1814397280&single=true",
+  },
+  colors: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=70370957&single=true",
+  },
+  fruit_and_veg: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1626957566&single=true",
+  },
+  hobbies: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1225906239&single=true",
+  },
+  family: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=0&single=true",
+  },
+  animals: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=934038451&single=true",
+  },
+  places: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1557824374&single=true",
+  },
+  taste: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=610825890&single=true",
+  },
+  home: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1530419016&single=true",
+  },
+  weather: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1054642637&single=true",
+  },
+  body: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1899740434&single=true",
+  },
+  clothes: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1167911748&single=true",
+  },
+  emotions: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1312510160&single=true",
+  },
+  transport: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=702917173&single=true",
+  },
+  school: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=213711171&single=true",
+  },
+  travel: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1043842277&single=true",
+  },
+  adjectives: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=531229956&single=true",
+  },
+  christmas: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=312614104&single=true",
+  },
+  days_of_the_week: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1757834596&single=true",
+  },
+  technology: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=173773526&single=true",
+  },
+  sports: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1028551617&single=true",
+  },
+  top_4000: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1084627403&single=true",
+    is_freq: true,
+  },
+  top_500: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=311547712&single=true",
+    is_freq: true,
+  },
+  top_500_to_1000: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=787094167&single=true",
+    is_freq: true,
+  },
+  top_1000_to_1500: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1647092263&single=true",
+    is_freq: true,
+  },
+  top_1500_to_2000: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=1599019213&single=true",
+    is_freq: true,
+  },
+  top_2000_to_3000: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=93796143&single=true",
+    is_freq: true,
+  },
+  top_3000_to_4000: {
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf2CHodZ6hYigjqYwqdIY8p_ZsJI5LFUTYhhie-69E4bkTBjpUXkhoYox5_4CDW3WEZEmN4xhECfpF/pubhtml?gid=84457090&single=true",
+    is_freq: true,
+  },
 };
 
 export default CategoryNames;
